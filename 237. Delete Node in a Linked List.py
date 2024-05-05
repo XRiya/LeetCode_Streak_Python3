85a431c39405da8237721b7baf68d303f975fe32
Riya Maurya <114230772+XRiya@users.noreply.github.com>
2024-05-05 18:10:15 +0530

    Create 237. Delete Node in a Linked List.py

---
+++ 237. Delete Node in a Linked List.py
@@ -0,0 +1,7 @@
+class Solution:
+   def deleteNode(self, node):
+       """
+       :type node: ListNode
+       :rtype: void Do not return anything, modify node in-place instead.
+       """
+       node.val, node.next=node.next.val, node.next.next
